bd634efc608f0d644e171a644392524bf26f6bcc
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-03 02:53:44 +0200

    Finished solution

---
+++ Lab2_z1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab2_z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite brzinu broda u čvorovima:");
+            string unos = Console.ReadLine();
+
+            int cvorovi = Int32.Parse(unos);
+            float kilometri = (float)(cvorovi * 1852) / 1000;
+
+            Console.WriteLine("Brzina broda u kilometrima je {0} km/h.", kilometri);
+        }
+    }
+}
